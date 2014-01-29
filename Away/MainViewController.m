9449ca36af3eaab553c3d1ca8a507ced77d22d46
Wesley Ide <wesley36@gmail.com>
2014-01-29 14:20:16 -0200

    Dois buttons no navigation controller

--- Away/MainViewController.m
+++ Away/MainViewController.m
@@ -36,7 +36,25 @@
 {
     [super viewDidLoad];
 
+    UIImage *imagefavoritesButton = [UIImage imageNamed:@"star-icon.png"];
+    CGSize newSize = CGSizeMake(40, 40);
+    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+    [imagefavoritesButton drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    imagefavoritesButton = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    UIBarButtonItem *favoritesButton = [[UIBarButtonItem alloc] initWithImage:imagefavoritesButton style:UIBarButtonItemStylePlain target:self action:@selector(favorites:)];
+
+    UIImage *imageSettingsButton = [UIImage imageNamed:@"settings-icon.png"];
+    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+    [imageSettingsButton drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    imageSettingsButton = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+    UIBarButtonItem *settingsButton = [[UIBarButtonItem alloc] initWithImage:imageSettingsButton style:UIBarButtonItemStylePlain target:self action:@selector(settings:)];
+    
+
     [self.navigationController setNavigationBarHidden:FALSE];
+    NSArray *buttons= [[NSArray alloc] initWithObjects:settingsButton,favoritesButton,nil];
+    self.navigationItem.rightBarButtonItems = buttons;
 
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapView:)];
     tap.numberOfTapsRequired = 1;
