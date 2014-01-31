d20c966c32d2a26b1b6adfe45091907c5610377b
mingatos <marcelo.m.toledo@gmail.com>
2014-01-31 21:25:20 -0200

    Added a FIFOQueue Category to NSMutableArray.

---
+++ Away/NSMutableArray+FIFOQueue.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+FIFOQueue.h
+//  Away
+//
+//  Created by Marcelo Toledo on 1/31/14.
+//  Copyright (c) 2014 Wesley Ide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray (FIFOQueue)
+
+@end
---
+++ Away/NSMutableArray+FIFOQueue.m
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+FIFOQueue.m
+//  Away
+//
+//  Created by Marcelo Toledo on 1/31/14.
+//  Copyright (c) 2014 Wesley Ide. All rights reserved.
+//
+
+#import "NSMutableArray+FIFOQueue.h"
+
+@implementation NSMutableArray (FIFOQueue)
+
+@end
